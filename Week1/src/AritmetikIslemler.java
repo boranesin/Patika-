e73cbec0b73d13a08409e4cc79dbaf0c7baefc5e
boranesinc <59020208+boranesin@users.noreply.github.com>
2024-04-28 21:07:28 +0300

    readme uzantısı eklendi

--- Week1/src/AritmetikIslemler.java
+++ Week1/src/AritmetikIslemler.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
 
 public class AritmetikIslemler {
+
+   // https://github.com/boranesin/PatikaPlus/blob/main/README.md
     public static void main(String[] args) {
+
+        // değişkenleri tanımlıyoruz
         int a, b, c, sonuc;
 
         Scanner input = new Scanner(System.in);
 
+        //  değişkenlere değer alıyoruz
         System.out.println("a verisini giriniz");
         a = input.nextInt();
         System.out.println("b verisini giriniz");
