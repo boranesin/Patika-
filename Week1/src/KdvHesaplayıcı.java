3bc3c19eab0df538b1cac44c35d7f3eec457fd31
boranesinc <59020208+boranesin@users.noreply.github.com>
2024-04-30 00:15:42 +0300

    Manav kasa hesaplama ve KDV hesaplama methodları eklendi

---
+++ Week1/src/KdvHesaplayıcı.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class KdvHesaplayıcı {
+    public static void main(String[] args) {
+        /* Java ile kullanıcıdan alınan para değerinin KDV'li fiyatını ve KDV tutarını hesaplayıp ekrana bastıran programı yazın.
+       (Not : KDV tutarını 18% olarak alın)
+        KDV'siz Fiyat = 10;
+KDV'li Fiyat = 11.8;
+KDV tutarı = 1.8;
+Ödev
+Eğer girilen tutar 0 ve 1000 TL arasında ise KDV oranı %18 , tutar 1000 TL'den büyük ise
+KDV oranını %8 olarak KDV tutarı hesaplayan program.*/
+        double tutar, kdvliTutar;
+        double kdvSifirBin = 0.18;
+        double kdvBinUzeri = 0.08;
+
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Lütfen KDV'si hesaplanacak tutarı giriniz: ");
+        tutar = input.nextDouble();
+        if (tutar < 1000){
+        kdvliTutar = tutar * (1 + kdvSifirBin);
+        } else { kdvliTutar =  tutar * (1 + kdvBinUzeri);
+        }
+        double kdvMiktari = kdvliTutar - tutar;
+        System.out.println("KDV'li tutar: " + kdvliTutar);
+        System.out.println("KDV : " + kdvMiktari);
+
+
+    }
+}
---
+++ Week1/src/ManavKasa.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class ManavKasa {
+    public static void main(String[] args) {
+        /*
+        Java ile kullanıcıların manavdan almış oldukları ürünlerin kilogram değerlerine göre toplam tutarını ekrana yazdıran programı yazın.
+Meyveler ve KG Fiyatları
+Armut : 2,14 TL
+Elma : 3,67 TL
+Domates : 1,11 TL
+Muz: 0,95 TL
+Patlıcan : 5,00 TL
+Armut Kaç Kilo ? :0
+Elma Kaç Kilo ? :1
+Domates Kaç Kilo ? :1
+Muz Kaç Kilo ? :2
+Patlıcan Kaç Kilo ? :3
+Toplam Tutar : 21.68 TL
+        */
+    double armut = 2.14;
+    double elma = 3.67;
+    double domates = 1.11;
+    double muzPudraSekersiz = 0.95;
+    double patlamican = 5.00;
+    double armutKilo, elmaKilo, domatesKilo, muzPudraSekersizKilo, patlamicanKilo;
+
+    Scanner input = new Scanner(System.in);
+
+        System.out.println("Lütfen almak istediğiniz ürünlerin miktarını KG olarak giriniz.");
+        System.out.println("Armut miktarı: ");
+        armutKilo = input.nextDouble();
+        System.out.println("Elma miktarı: ");
+        elmaKilo = input.nextDouble();
+        System.out.println("Domates miktarı: ");
+        domatesKilo = input.nextDouble();
+        System.out.println("Muz miktarı (Lütfen pudra şekersiz olanını giriniz): ");
+        muzPudraSekersizKilo = input.nextDouble();
+        System.out.println("Patlamıcan miktarı: ");
+        patlamicanKilo = input.nextDouble();
+        double toplamTutar = armutKilo*armut + elmaKilo*elma + domatesKilo*domates + muzPudraSekersizKilo*muzPudraSekersiz + patlamicanKilo*patlamican;
+        System.out.println("Toplam tutar (kdvsiz): " + toplamTutar);
+
+    }
+}
